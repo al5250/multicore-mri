0bd6681e3ae98d8037ab97a529e3ce80d16357da
Alexander Lin <alin5250@gmail.com>
2021-05-17 10:48:09 -0400

    deleted more unnecessary files

--- multicore/algorithm/__init__.py
+++ multicore/algorithm/__init__.py
@@ -1,26 +1,10 @@
 from multicore.algorithm.algorithm import ReconstructionAlgorithm
 from multicore.algorithm.zero_fill import ZeroFilling
-from multicore.algorithm.l1_conj_grad import L1ConjugateGradient
 from multicore.algorithm.bayesian_cs import BayesianCompressedSensing
-from multicore.algorithm.bayesian_cs_multicoil import (
-    BayesianCompressedSensingMultiCoil,
-    BayesianCompressedSensingMultiCoilForward
-)
-from multicore.algorithm.generalized_bcs import GeneralizedBCS
-from multicore.algorithm.multicoil_bcs import MulticoilBCS
-from multicore.algorithm.sense import SENSE
-from multicore.algorithm.multisense import MultiSENSE
 
 
 __all__ = [
     'ReconstructionAlgorithm',
     'ZeroFilling',
-    'L1ConjugateGradient',
     'BayesianCompressedSensing',
-    'BayesianCompressedSensingMultiCoil'
-    'BayesianCompressedSensingMultiCoilForward',
-    'GeneralizedBCS',
-    'MulticoilBCS',
-    'SENSE'
-    'MultiSENSE'
 ]
--- multicore/dataset/__init__.py
+++ multicore/dataset/__init__.py
@@ -1,53 +1,7 @@
-from multicore.dataset.dataset import MRIDataset
 from multicore.dataset.atlas import AtlasDataset, AtlasDatasetPowerRule
-from multicore.dataset.complex_sl import (
-    ComplexSheppLoganDataset,
-    ComplexSheppLoganDatasetPowerRule
-)
-from multicore.dataset.single_coil import (
-    SingleCoilInVivoDataset,
-    SingleCoilInVivoDatasetPowerRule
-)
-from multicore.dataset.sim_8ch import (
-    Sim8ChannelDataset,
-    Sim8ChannelDatasetPowerRule
-)
-from multicore.dataset.sim_8ch_under import (
-    Sim8ChannelUnderDataset,
-    Sim8ChannelUnderDatasetPowerRule
-)
-from multicore.dataset.multi_coil import (
-    MultiCoilInVivoDataset,
-    MultiCoilInVivoDatasetPowerRule
-)
-from multicore.dataset.atlas128 import (
-    Atlas128Dataset,
-    Atlas128DatasetPowerRule
-)
-from multicore.dataset.sl import (
-    SheppLoganDataset,
-    SheppLoganDatasetPowerRule
-)
-from multicore.dataset.sl_multi_coil import (
-    SheppLoganMultiCoilDataset
-)
 
 
 __all__ = [
-    'MRIDataset',
     'AtlasDataset',
-    'AtlasDatasetPowerRule',
-    'ComplexSheppLoganDataset',
-    'ComplexSheppLoganDatasetPowerRule',
-    'Sim8ChannelDataset',
-    'Sim8ChannelDatasetPowerRule'
-    'Sim8ChannelUnderDataset',
-    'Sim8ChannelUnderDatasetPowerRule',
-    'MultiCoilInVivoDataset',
-    'MultiCoilInVivoDatasetPowerRule',
-    'Atlas128Dataset',
-    'Atlas128DatasetPowerRule',
-    'SheppLoganDataset',
-    'SheppLoganDatasetPowerRule'
-    'SheppLoganMultiCoilDataset'
+    'AtlastDatasetPowerRule'
 ]
